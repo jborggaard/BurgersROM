c38e01754dc30b16e18543f003b8d527482e4ad4
Jeff Borggaard <jborggaard@gmail.com>
2022-02-14 15:06:28 -0500

    Introduced change of variable in openLoop.m

--- openLoop.m
+++ openLoop.m
@@ -21,3 +21,26 @@
   mesh(xNodes,T,Z)
   xlabel('x'); ylabel('time')
   title('Open Loop Simulation')
+
+%  Alternate by change of variable (to remove M matrix)
+  sqM = sqrtm(full(M));
+  sqMinv = inv(sqM);
+  
+  Ac = sqMinv*A*sqMinv;                  %#ok
+  Bc = sqMinv*B;                         %#ok
+  Nc = kroneckerRight(sqMinv*N,sqMinv);  %#ok
+  Qc = eye(n);  R = r0*eye(m);
+  
+  rhs_open = @(t,x) [ Aopen*x(1:end-1) + Nopen*kron(x(1:end-1),x(1:end-1));...
+                        x(1:end-1).'*Qc*x(1:end-1) ];
+                        
+  [t,x] = ode15s( rhs_open, [0 T], [x0;0], options );  
+  figure(1); hold on
+  Z = x(:,1:end-1)*sqMinv; Z(:,end+1) = Z(:,1);  %#ok
+  mesh(xNodes,t,Z)
+  xlabel('x'); ylabel('time')
+  title('Open Loop Simulation')
+  view([1 -1 1])
+  axis([0 1 -0.1 5 -.4 .6]) 
+  
+  fprintf('Open Loop Cost (0,T) is %g\n\n',x(end,end));
